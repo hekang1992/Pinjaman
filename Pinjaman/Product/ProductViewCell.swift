ca8d0a5856823367ab2ad6c14a59b0e6fad68fe8
hekang1992 <hekang19921111@hotmail.com>
2026-01-28 09:24:22 +0800

    a

--- Pinjaman/Product/ProductViewCell.swift
+++ Pinjaman/Product/ProductViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import Kingfisher
 
 class ProductViewCell: UITableViewCell {
     
@@ -15,6 +16,8 @@ class ProductViewCell: UITableViewCell {
             guard let model = model else { return }
             let nascorium = model.nascorium ?? ""
             typeImageView.image = nascorium == "1" ? UIImage(named: "cmp_lis_l_r_image") : UIImage(named: "cpo_lis_l_r_image")
+            logoImageView.kf.setImage(with: URL(string: model.tenuot ?? ""))
+            nameLabel.text = model.asform ?? ""
         }
     }
     
@@ -41,6 +44,22 @@ class ProductViewCell: UITableViewCell {
         typeImageView.image = UIImage(named: "cpo_lis_l_r_image")
         return typeImageView
     }()
+    
+    lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.textAlignment = .left
+        nameLabel.textColor = UIColor.init(hexString: "#333333")
+        nameLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        return nameLabel
+    }()
+    
+    lazy var logoImageView: UIImageView = {
+        let logoImageView = UIImageView()
+        logoImageView.layer.cornerRadius = 5
+        logoImageView.layer.masksToBounds = true
+        logoImageView.backgroundColor = .red
+        return logoImageView
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -49,6 +68,8 @@ class ProductViewCell: UITableViewCell {
         
         contentView.addSubview(bgView)
         bgView.addSubview(oneImageView)
+        oneImageView.addSubview(logoImageView)
+        oneImageView.addSubview(nameLabel)
         bgView.addSubview(twoImageView)
         twoImageView.addSubview(typeImageView)
         bgView.snp.makeConstraints { make in
@@ -60,6 +81,16 @@ class ProductViewCell: UITableViewCell {
             make.centerY.equalToSuperview()
             make.left.equalToSuperview().offset(16.pix())
             make.size.equalTo(CGSize(width: 290.pix(), height: 66.pix()))
+        }
+        logoImageView.snp.makeConstraints { make in
+            make.width.height.equalTo(54.pix())
+            make.left.equalToSuperview().offset(5.pix())
+            make.centerY.equalToSuperview()
+        }
+        nameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.height.equalTo(30.pix())
+            make.left.equalTo(logoImageView.snp.right).offset(21.pix())
         }
         twoImageView.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
